c7f0c48a855273fccae3c95572af36296c601813
Dup4 <lyuzhi.pan@gmail.com>
2021-12-15 21:51:01 +0800

    feat: dump source code and runs

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import base64
 import os
 import json
-import time
 import logging
 import requests
 import shutil
@@ -41,11 +40,34 @@ def urlJoin(url, *args):
 
 
 class Config:
-    def getConfigWithDefaultCalue(self, config_dict, key, default_value):
+    @staticmethod
+    def getConfigWithDefaultCalue(config_dict, key, default_value):
         if key in config_dict.keys():
             return config_dict[key]
 
         return default_value
+
+    class ExportedData:
+        def __init__(self, exported_data_dict):
+            self.domjudge_api = Config.getConfigWithDefaultCalue(
+                exported_data_dict, 'domjudge_api', True)
+
+            # Since the export of event-feed may be a bit slow
+            # we do not export by defaut
+            self.event_feed = Config.getConfigWithDefaultCalue(
+                exported_data_dict, 'event_feed', False)
+
+            self.runs = Config.getConfigWithDefaultCalue(
+                exported_data_dict, 'runs', False)
+
+            self.source_code = Config.getConfigWithDefaultCalue(
+                exported_data_dict, 'source_code', False)
+
+            self.ghost_dat_data = Config.getConfigWithDefaultCalue(
+                exported_data_dict, 'ghost_dat_data', True)
+
+            self.resolver_data = Config.getConfigWithDefaultCalue(
+                exported_data_dict, 'resolver_data', True)
 
     def __init__(self, config_dict):
         self.base_file_path = self.getConfigWithDefaultCalue(
@@ -68,11 +90,15 @@ class Config:
         self.score_in_seconds = self.getConfigWithDefaultCalue(
             config_dict, 'score_in_seconds', False)
 
+        # Since Ghost Dat Data with Chinese team names may be garbled
+        # when imported into Codeforces,
+        # We found that if some dummy Russian teams are added, it may works.
+        # This configuration item is only for exporting ghost dat data
         self.add_dummy_russian_team = self.getConfigWithDefaultCalue(
             config_dict, 'add_dummy_russian_team', False)
 
-        self.save_domjudge_api_to_file = self.getConfigWithDefaultCalue(
-            config_dict, 'save_domjudge_api_to_file', True)
+        self.exported_data = Config.ExportedData(
+            config_dict['exported_data'] if 'exported_data' in config_dict.keys() else {})
 
 
 def loadConfig():
@@ -97,6 +123,17 @@ def initLogging():
     logger.addHandler(consoleHandler)
 
 
+def sendRequest(url, params={}):
+    res = requests.get(url=url, headers=headers, params=params)
+
+    if res.status_code != 200:
+        logger.error('An error occurred during request GET {}, errcode:{}'.format(
+            url, res.status_code))
+        exit()
+
+    return res
+
+
 def requestJson(endpoint, params={}):
     if default_config.base_file_path == '':
         url = urlJoin(base_url, str(default_config.cid))
@@ -105,13 +142,8 @@ def requestJson(endpoint, params={}):
             url = urlJoin(url, endpoint)
 
         logger.info('GET {}'.format(url))
-        res = requests.get(url=url, params=params,
-                           headers=headers)
-        logger.info('Result Code: {}'.format(res.status_code))
 
-        if res.status_code != 200:
-            logger.error('An error occurred during request.')
-            exit()
+        res = sendRequest(url, params)
 
         return res.content.decode('unicode-escape')
     else:
@@ -122,11 +154,10 @@ def requestJson(endpoint, params={}):
 
 
 def requestJsonAndSave(endpoint, filename, params={}):
-    sub_dir_path = 'domjudge-api'
     content = requestJson(
         endpoint if default_config.base_file_path == '' else filename, params=params)
 
-    if default_config.save_domjudge_api_to_file:
+    if default_config.exported_data.domjudge_api:
         ensureDir(os.path.join(default_config.saved_dir, sub_dir_path))
         outputToFile(os.path.join(sub_dir_path, filename), content)
 
@@ -178,64 +209,103 @@ def isObservers(team):
     return False
 
 
-def getResolverData(contest, teams, submissions, problems_dict):
-    resolver_data = {}
-    resolver_data["contest_name"] = contest["formal_name"]
-    resolver_data["problem_count"] = len(problems_dict)
-    resolver_data["frozen_seconds"] = getSeconds(
-        contest["duration"]) - getSeconds(contest["scoreboard_freeze_duration"])
+def dumpDOMjudgeAPI():
+    global contest, awards, scoreboard, groups, judgements
+    global judgement_types, languages, organizations, problems, teams, submissions
+    global clarifications, event_feed
 
-    users = {}
-    solutions = {}
+    contest = requestJsonAndSave('', 'contest.json')
+    awards = requestJsonAndSave('awards', 'awards.json')
+    scoreboard = requestJsonAndSave('scoreboard', 'scoreboard.json')
 
-    for team in teams:
-        item = {}
-        item['name'] = team['name']
-        item['college'] = team['affiliation']
-        item['is_exclude'] = isObservers(team)
+    groups = requestJsonAndSave('groups', 'groups.json')
+    judgements = requestJsonAndSave('judgements', 'judgements.json')
+    judgement_types = requestJsonAndSave(
+        'judgement-types', 'judgement-types.json')
+    languages = requestJsonAndSave('languages', 'languages.json')
+    organizations = requestJsonAndSave('organizations', 'organizations.json')
+    problems = requestJsonAndSave('problems', 'problems.json')
+    teams = requestJsonAndSave('teams', 'teams.json')
+    submissions = requestJsonAndSave('submissions', 'submissions.json')
 
-        users[team['id']] = item
+    clarifications = requestJsonAndSave(
+        'clarifications', 'clarifications.json')
 
-    submission_index = 1
-    for submission in submissions:
-        id = submission['id']
-        verdict = submission['verdict']
-
-        if verdict != 'AC' and verdict != 'CE':
-            verdict = 'WA'
-
-        if verdict == 'CE':
-            continue
-
-        item = {}
-        item['submitted_seconds'] = getSubmissionTimestamp(
-            submission['contest_time'])
-
-        # TOO LATE submission
-        if item['submitted_seconds'] > getSeconds(contest["duration"]):
-            continue
-
-        item['user_id'] = submission['team_id']
-
-        if item['user_id'] not in users.keys():
-            continue
-
-        # Count from 1
-        item['problem_index'] = str(ord(
-            problems_dict[submission['problem_id']]['label']) - ord('A') + 1)
-
-        item['verdict'] = verdict
-
-        solutions[submission_index] = item
-        submission_index += 1
-
-    resolver_data['solutions'] = solutions
-    resolver_data['users'] = users
-
-    outputToFile('resolver.json', objectToJSONString(resolver_data))
+    if default_config.exported_data.event_feed:
+        event_feed = requestJsonAndSave(
+            'event-feed', 'event-feed.json', {'stream': False, 'strict': True})
 
 
-def getDATData(contest, teams_dict, submissions, problems_dict):
+def dumpRuns():
+    if default_config.base_url == '':
+        return
+
+    if default_config.exported_data.runs:
+        runs = requestJsonAndSave('runs?limit=10000', 'runs.1.json')
+
+        i = 2
+        saved_filename = ''
+
+        while len(runs) != 0:
+            endpoint = 'runs?limit=10000&first_id={}'.format(
+                str(int(runs[-1]['id'])+1))
+
+            saved_filename = 'runs.{}.json'.format(str(i))
+
+            runs = requestJsonAndSave(endpoint, saved_filename)
+            i = i + 1
+
+        # Obviously, there will be an empty JSON file
+        os.remove(os.path.join(default_config.saved_dir,
+                  sub_dir_path, saved_filename))
+
+
+def downloadSourceCodeFiles(sid):
+    url = urlJoin(base_url, str(default_config.cid),
+                  'submissions', str(sid), 'files')
+    res = sendRequest(url)
+
+    with open(os.path.join(submissions_dir, str(sid), 'files.zip'), 'wb') as f:
+        f.write(res.content)
+
+
+def downloadSourceCodeJson(sid):
+    url = urlJoin(base_url, str(default_config.cid),
+                  'submissions', str(sid), 'source-code')
+    res = sendRequest(url)
+
+    content = res.content.decode('unicode-escape')
+
+    with open(os.path.join(submissions_dir, str(sid), 'source-code.json'), 'w') as f:
+        f.write(content)
+
+    return content
+
+
+def dumpSourceCode():
+    if default_config.base_url == '':
+        return
+
+    if default_config.exported_data.source_code:
+        total = len(submissions)
+        i = 0
+
+        ensureDir(submissions_dir)
+
+        for submission in submissions:
+            submission_id = submission['id']
+
+            ensureDir(os.path.join(submissions_dir, submission_id))
+
+            downloadSourceCodeFiles(submission_id)
+            downloadSourceCodeJson(submission_id)
+
+            i = i + 1
+            if i % 50 == 0 or i == total:
+                logger.info('Submissions {}/{}'.format(str(i), str(total)))
+
+
+def getGhostDATData(contest, teams_dict, submissions, problems_dict):
     verdict_mapping = {
         'CE': 'CE',
         'MLE': 'ML',
@@ -310,44 +380,68 @@ def getDATData(contest, teams_dict, submissions, problems_dict):
     outputToFile('contest.dat', dat_data)
 
 
-def main():
-    global headers, base_url, submissions_dir
-    global contest, awards, scoreboard, groups, judgements
-    global judgement_types, languages, organizations, problems, teams, submissions
+def getResolverData(contest, teams, submissions, problems_dict):
+    resolver_data = {}
+    resolver_data["contest_name"] = contest["formal_name"]
+    resolver_data["problem_count"] = len(problems_dict)
+    resolver_data["frozen_seconds"] = getSeconds(
+        contest["duration"]) - getSeconds(contest["scoreboard_freeze_duration"])
+
+    users = {}
+    solutions = {}
+
+    for team in teams:
+        item = {}
+        item['name'] = team['name']
+        item['college'] = team['affiliation']
+        item['is_exclude'] = isObservers(team)
+
+        users[team['id']] = item
+
+    submission_index = 1
+    for submission in submissions:
+        id = submission['id']
+        verdict = submission['verdict']
+
+        if verdict != 'AC' and verdict != 'CE':
+            verdict = 'WA'
+
+        if verdict == 'CE':
+            continue
+
+        item = {}
+        item['submitted_seconds'] = getSubmissionTimestamp(
+            submission['contest_time'])
+
+        # TOO LATE submission
+        if item['submitted_seconds'] > getSeconds(contest["duration"]):
+            continue
+
+        item['user_id'] = submission['team_id']
+
+        if item['user_id'] not in users.keys():
+            continue
+
+        # Count from 1
+        item['problem_index'] = str(ord(
+            problems_dict[submission['problem_id']]['label']) - ord('A') + 1)
+
+        item['verdict'] = verdict
+
+        solutions[submission_index] = item
+        submission_index += 1
+
+    resolver_data['solutions'] = solutions
+    resolver_data['users'] = users
+
+    outputToFile('resolver.json', objectToJSONString(resolver_data))
+
+
+def dump3rdData():
+    if not default_config.exported_data.ghost_dat_data and not default_config.exported_data.resolver_data:
+        return
+
     global problems_dict, groups_dict, teams_dict
-
-    loadConfig()
-    initLogging()
-
-    headers = {'Authorization': 'Basic ' +
-               base64.encodebytes(default_config.userpwd.encode('utf-8')).decode('utf-8').strip()}
-
-    base_url = urlJoin(default_config.base_url, 'api',
-                       default_config.api_version, 'contests')
-
-    submissions_dir = os.path.join(default_config.saved_dir, 'submissions')
-
-    if os.path.exists(default_config.saved_dir):
-        shutil.rmtree(default_config.saved_dir)
-
-    ensureDir(default_config.saved_dir)
-
-    contest = requestJsonAndSave('', 'contest.json')
-    awards = requestJsonAndSave('awards', 'awards.json')
-    scoreboard = requestJsonAndSave('scoreboard', 'scoreboard.json')
-    clarifications = requestJsonAndSave(
-        'clarifications', 'clarifications.json')
-    # event_feed = requestJsonAndSave(
-    #     'event-feed', 'event-feed.json', {'stream': False, 'strict': True})
-    groups = requestJsonAndSave('groups', 'groups.json')
-    judgements = requestJsonAndSave('judgements', 'judgements.json')
-    judgement_types = requestJsonAndSave(
-        'judgement-types', 'judgement-types.json')
-    languages = requestJsonAndSave('languages', 'languages.json')
-    organizations = requestJsonAndSave('organizations', 'organizations.json')
-    problems = requestJsonAndSave('problems', 'problems.json')
-    teams = requestJsonAndSave('teams', 'teams.json')
-    submissions = requestJsonAndSave('submissions', 'submissions.json')
 
     problems_dict = {}
     for problem in problems:
@@ -363,9 +457,40 @@ def main():
 
     addVerdict(submissions, judgements)
 
-    getResolverData(contest, teams, submissions,
-                    problems_dict)
-    getDATData(contest, teams_dict, submissions, problems_dict)
+    if default_config.exported_data.ghost_dat_data:
+        getGhostDATData(contest, teams_dict, submissions, problems_dict)
+
+    if default_config.exported_data.resolver_data:
+        getResolverData(contest, teams, submissions,
+                        problems_dict)
+
+
+def main():
+    loadConfig()
+    initLogging()
+
+    global headers, base_url, sub_dir_path, submissions_dir
+
+    headers = {'Authorization': 'Basic ' +
+               base64.encodebytes(default_config.userpwd.encode('utf-8')).decode('utf-8').strip()}
+
+    base_url = urlJoin(default_config.base_url, 'api',
+                       default_config.api_version, 'contests')
+
+    sub_dir_path = 'domjudge-api'
+
+    submissions_dir = os.path.join(
+        default_config.saved_dir, sub_dir_path, 'submissions')
+
+    if os.path.exists(default_config.saved_dir):
+        shutil.rmtree(default_config.saved_dir)
+
+    ensureDir(default_config.saved_dir)
+
+    dumpDOMjudgeAPI()
+    dumpRuns()
+    dumpSourceCode()
+    dump3rdData()
 
 
 if __name__ == '__main__':
