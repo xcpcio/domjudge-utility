dccabd5461c23b64f4588e7c0276d1fd40221dba
Dup4 <lyuzhi.pan@gmail.com>
2023-04-13 09:06:09 +0800

    feta: replace_html for board-cache
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- board-cache/config.py
+++ board-cache/config.py
@@ -3,15 +3,19 @@ import os
 
 
 class FetchItem:
-    def __init__(self, url_suffix, dir_name):
+    def __init__(self, url_suffix, dir_name, show_name, show_url):
         self.url_suffix = url_suffix
         self.dir_name = dir_name
+        self.show_name = show_name
+        self.show_url = show_url
 
 
 class Config:
-    def __init__(self, url, saved_dir, fetch_list):
+    def __init__(self, url, url_prefix, saved_dir, assets_url, fetch_list):
         self.url = url
+        self.url_prefix = url_prefix
         self.saved_dir = saved_dir
+        self.assets_url = assets_url
         self.fetch_list = [FetchItem(**item) for item in fetch_list]
 
 
--- board-cache/main.py
+++ board-cache/main.py
@@ -1,11 +1,76 @@
 import os
 import requests
+import htmlmin
 
 import config
 from logger import init_logger
 
 global_config = config.load_config()
 logger = init_logger()
+
+
+def replace_html(html: str, current_fetch_item: config.FetchItem) -> str:
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    element = soup.find('a', {'href': global_config.url_prefix + "public"})
+    if element is not None:
+        element.extract()
+
+    element = soup.find('div', {'id': 'menuDefault'}).find('ul')
+    if element is not None:
+        element.extract()
+
+    element = soup.find('a', {'href': global_config.url_prefix + "login"})
+    if element is not None:
+        element.extract()
+
+    element = soup.find('div', {'class': 'dropdown'})
+    if element is not None:
+        element.insert_after(soup.new_tag('br'))
+        element.extract()
+
+    html = str(soup)
+
+    flag_div = '<div class="collapse navbar-collapse" id="menuDefault">'
+
+    board_menu = '''
+<ul class="navbar-nav mr-auto">
+    '''
+
+    for item in global_config.fetch_list:
+        board_menu += '''
+    <li class="nav-item {active}">
+        <a class="nav-link" href="{show_url}">{show_name}</a>
+    </li>
+        '''.format(
+            active="active" if item.dir_name == current_fetch_item.dir_name else "",
+            show_url=item.show_url,
+            show_name=item.show_name,
+        )
+
+    board_menu += "</ul>"
+
+    html = html.replace(flag_div, flag_div + board_menu)
+
+    html = html.replace("'" + global_config.url_prefix +
+                        "public'", "'" + current_fetch_item.show_url + "'")
+
+    return html
+
+
+def replace_assets_url(html: str) -> str:
+    if global_config.assets_url is None:
+        return html
+
+    html = html.replace(global_config.url_prefix, global_config.assets_url)
+    return html
+
+
+def minify_html(html: str) -> str:
+    html = htmlmin.minify(html)
+    return html
 
 
 def fetch_html(fetch_item: config.FetchItem):
@@ -23,9 +88,14 @@ def fetch_html(fetch_item: config.FetchItem):
     if not os.path.exists(saved_dir):
         os.makedirs(saved_dir)
 
+    html = resp.text
+    html = replace_html(html, fetch_item)
+    html = replace_assets_url(html)
+    html = minify_html(html)
+
     saved_file = os.path.join(saved_dir, "index.html")
     with open(saved_file, "w") as f:
-        f.write(resp.text)
+        f.write(html)
 
 
 def main():
